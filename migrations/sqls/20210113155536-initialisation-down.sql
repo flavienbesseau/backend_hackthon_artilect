2a08187af013585f4cecf29848a426812c5e0862
FlavienBesseau <besseau.flavien@gmail.com>
2021-01-14 00:00:21 +0100

    insert data in the database

--- migrations/sqls/20210113155536-initialisation-down.sql
+++ migrations/sqls/20210113155536-initialisation-down.sql
@@ -1,1 +1,13 @@
 /* Replace with your SQL commands */
+
+SET foreign_key_checks=0;
+DROP TABLE IF EXISTS `competence`;
+DROP TABLE IF EXISTS `competence_machine`;
+DROP TABLE IF EXISTS `machine`;
+DROP TABLE IF EXISTS `project`;
+DROP TABLE IF EXISTS `project_competence`;
+DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `user_competence`;
+DROP TABLE IF EXISTS `user_machine`;  
+DROP TABLE IF EXISTS `user_project`;
+SET foreign_key_checks=1;
