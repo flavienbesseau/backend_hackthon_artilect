31b3e417b7064a4dcb22705a088383b762d4351e
FlavienBesseau <besseau.flavien@gmail.com>
2021-01-14 15:46:16 +0100

    corrections database + 3 more controllerss

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,18 @@ const router = express.Router();
 
 const projects = require("./projects");
 const competences = require("./competences");
-const machines = require("./machines");
 const users = require("./users");
+const machines = require("./machines");
 const projectMachineUser = require("./projectMachineUser");
+const personality = require("./personality");
+const userProject = require("./userProject");
 
 router.use("/projects", projects);
 router.use("/competences", competences);
 router.use("/users", users);
 router.use("/machines", machines);
 router.use("/projectMachineUser", projectMachineUser);
+router.use("/personalities", personality);
+router.use("/userProjects", userProject);
 
 module.exports = router;
