409163dd5c8d892f30a8688d0aeab5b62dbaf498
FlavienBesseau <besseau.flavien@gmail.com>
2021-01-15 10:00:47 +0100

    add machine controller

---
+++ routes/machine.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const router = express.Router();
+const { connection } = require("../config");
+
+router.get("/:id", (req, res) => {
+  connection.query(
+    "SELECT * from machine WHERE id = ? ",
+    [req.params.id],
+
+    (err, results) => {
+      if (err || results.length === 0) {
+        res.status(500).send("Error retrieving data");
+      } else {
+        res.status(200).json(results);
+      }
+    }
+  );
+});
+
+module.exports = router;
--- routes/machines.js
+++ routes/machines.js
@@ -3,9 +3,10 @@ const express = require("express");
 const router = express.Router();
 const { connection } = require("../config");
 
-router.get("/:id", (req, res) => {
+router.get("/", (req, res) => {
   connection.query("SELECT * from machine", (err, results) => {
     if (err || results.length === 0) {
+      console.log(err);
       res.status(500).send("Error retrieving data");
     } else {
       res.status(200).json(results);
