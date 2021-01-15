02b0f4d44c3e24219c7f5c0aede708163d4bfb83
FlavienBesseau <besseau.flavien@gmail.com>
2021-01-15 01:39:23 +0100

    change project controller

--- routes/projects.js
+++ routes/projects.js
@@ -5,7 +5,7 @@ const { connection } = require("../config");
 
 router.get("/", (req, res) => {
   connection.query(
-    "SELECT p.name AS project_name, p.status, p.description, p.image, p.discord_link AS project_discord_link, p.image, m.url_photo FROM project AS p LEFT JOIN machine AS m ON m.id = p.fk_machine_id",
+    "SELECT p.id, p.name AS project_name, p.status, p.description, p.image, p.discord_link AS project_discord_link, p.image, m.url_photo FROM project AS p LEFT JOIN machine AS m ON m.id = p.fk_machine_id",
     (err, results) => {
       if (err || results.length === 0) {
         res.status(500).send("Error retrieving data");
